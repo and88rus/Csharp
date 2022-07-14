0e7fa1d740015b3cd313a1c25b9b7b121531f907
and88rus <nest160802@gmail.com>
2022-07-14 06:30:14 +0300

    changes 14.07.2022 at 6:30

---
+++ morethan0/Program.cs
@@ -0,0 +1,27 @@
+﻿void morethan0(int a) 
+{
+ int [] m = new int [a];
+ Console.WriteLine("Введите элементы:");
+ for (int i=0; i<a; i++)
+ {
+   Console.WriteLine(" ");
+   m[i] = Convert.ToInt32(Console.ReadLine());
+ };
+ Console.Clear();
+ Console.WriteLine(" ");
+int k=0;
+ for (int i=0; i<a; i++)
+ {
+   if (m[i]>0){
+    k=k+1;
+   };
+ };
+ Console.WriteLine($"Кол-во введенных чисел, больших 0: {k}");
+};
+Console.WriteLine(" ");
+Console.WriteLine("Введите число M:");
+Console.WriteLine(" ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+morethan0(m);
+Console.WriteLine(" ");
---
+++ pointofcross/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задание можно переформулировать следующим образом: решите систему линенйых уравнений { (1) y=k1*x+b1 (2) y=k2*x+b2, которую 
+// в свою очередь можно решить по формулам Крамера.
+void kramer(double b1, double k1, double b2, double k2)
+{
+ if (k1==k2)
+ {
+   Console.WriteLine($"Прямые y={k1}*x+{b1} и y={k2}*x+{b2} при значениях  b1={b2},k1={k1}, b2={b2}, k2={k2} не имеют точку пересечения.");
+ }
+ else 
+ {
+ double x=(b1-b2)/(-k1+k2);
+ double y=(-k1*b2+k2*b1)/(-k1+k2); 
+  Console.WriteLine(" ");
+ Console.WriteLine($"Прямые y={k1}*x+{b1} и y={k2}*x+{b2} при значениях b1={b2},k1={k1}, b2={b2}, k2={k2} имеют точку пересечения: ({x}.{y})");
+ };
+};
+Console.WriteLine(" ");
+Console.WriteLine("Введите значение параметра b1:");
+Console.WriteLine(" ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" ");
+Console.WriteLine("Введите значение параметра k1:");
+Console.WriteLine(" ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" ");
+Console.WriteLine("Введите значение параметра b2:");
+Console.WriteLine(" ");
+double c = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" ");
+Console.WriteLine("Введите значение параметра k2:");
+Console.WriteLine(" ");
+double d = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" ");
+kramer(a,b,c,d);

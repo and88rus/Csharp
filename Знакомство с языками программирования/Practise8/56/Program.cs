8b8fd878cf613451f8b40c4b1249ceae311d0969
and88rus <nest160802@gmail.com>
2022-07-25 00:41:52 +0300

    начал выполнять сборник из практических заданий под номером 8

---
+++ Знакомство с языками программирования/Practise8/56/Program.cs
@@ -0,0 +1,51 @@
+﻿void AxBArray (int a,int b,int x1,int x2)
+{
+int [,] m = new int[a,b];
+for (int i=0;i<a;i++)
+{
+    for (int j=0;j<b;j++)
+    {
+      m[i,j]=m[i,j]=Int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine(" ");
+Console.WriteLine($"Созданный массив размера {a}x{b} из вещественных чисел следующий:");
+Console.WriteLine(" ");
+for (int i=0;i<a;i++)
+{
+    for (int j=0;j<b;j++)
+    {
+      Console.Write($"{m[i,j]} ");
+    }
+    Console.WriteLine();
+}
+}; 
+void LeastSumOfLineOfAxBArray ()
+{
+
+};
+Console.WriteLine(" ");
+Console.WriteLine("Введите кол-во строк массива:");
+Console.WriteLine(" ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
+Console.Clear();
+Console.WriteLine("Введите кол-во столбцов массива:");
+Console.WriteLine(" ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
+Console.Clear();
+Console.Clear();
+Console.WriteLine("Ввод диапозона значений (x1,x2).");
+Console.WriteLine(" ");
+Console.WriteLine("(1) Введите число x1:");
+Console.WriteLine(" ");
+int y3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
+Console.Clear();
+Console.WriteLine("(2) Введите число x2:");
+Console.WriteLine(" ");
+int y4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
+Console.Clear();
+AxBArray (y1,y2,y3,y4);

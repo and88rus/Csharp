729e042ac61403b2976b5799f54cfc89cfb1ea6c
and88rus <nest160802@gmail.com>
2022-08-03 18:08:13 +0300

    new file

--- tests/test3/Program.cs
+++ tests/test3/Program.cs
@@ -1,20 +1,22 @@
 ﻿
-int a = Convert.ToInt32(Console.ReadLine());
+int line_of_m = Convert.ToInt32(Console.ReadLine());
 
-int b = Convert.ToInt32(Console.ReadLine());
+int col_of_m = Convert.ToInt32(Console.ReadLine());
 
-int c = Convert.ToInt32(Console.ReadLine());
+int page_of_m = Convert.ToInt32(Console.ReadLine());
 
-int [,,] m= new int [a,b,c];
+Random rnd = new Random();
 
-for (int i=0;i<a;i++) 
+int [,,] m= new int [line_of_m,col_of_m,page_of_m];
+
+for (int i=0;i<line_of_m;i++) 
 {
-for (int j=0;j<b;j++)
+for (int j=0;j<col_of_m;j++)
  { 
-  for (int t=0; t<c;t++)
+  for (int t=0; t<page_of_m;t++)
   {
   Console.Write ($"Введите {i},{j},{t}-ый элемент:");
-m[i,j,t]=Convert.ToInt32(Console.ReadLine());
+m[i,j,t]=rnd.Next(9,100);
   }
  }
 Console.WriteLine (" ");
@@ -24,13 +26,44 @@ Console.WriteLine(" ");
 Console.WriteLine(" ... ");
 Console.WriteLine(" ");
 
-for (int i=0;i<a;i++)
+for (int i=0;i<line_of_m;i++)
 {
- for (int j=0;j<b;j++)
+ for (int j=0;j<col_of_m;j++)
   { 
-   for (int t=0; t<c; t++)
+   for (int t=0; t<page_of_m; t++)
    {
        Console.Write($" {m[i,j,t]}{(i,j,t)} "); 
+   }
+    Console.WriteLine (" ");
+  }
+  Console.WriteLine (" ");
+}
+
+int [,,] mcopy = new int [line_of_m,col_of_m,page_of_m];
+
+for (int i=0;i<line_of_m;i++) 
+{
+for (int j=0;j<col_of_m;j++)
+ { 
+  for (int t=0; t<page_of_m;t++)
+  {
+mcopy[i,j,t]=m[i,j,t];
+  }
+ }
+Console.WriteLine (" ");
+}
+
+Console.WriteLine(" ");
+Console.WriteLine(" ... ");
+Console.WriteLine(" ");
+
+for (int i=0;i<line_of_m;i++)
+{
+ for (int j=0;j<col_of_m;j++)
+  { 
+   for (int t=0; t<page_of_m; t++)
+   {
+       Console.Write($" {mcopy[i,j,t]}{(i,j,t)} "); 
    }
     Console.WriteLine (" ");
   }
@@ -40,6 +73,58 @@ for (int i=0;i<a;i++)
 Console.WriteLine(" ");
 Console.WriteLine(" ... ");
 Console.WriteLine(" ");
+
+int [,,] mcout = new int [line_of_m,col_of_m,page_of_m];
+
+for (int i=0;i<line_of_m;i++) 
+{
+for (int j=0;j<col_of_m;j++)
+ { 
+  for (int t=0; t<page_of_m;t++)
+  {
+    mcout[i,j,t]=0;
+       for (int x=0;x<line_of_m;x++) 
+       {
+        for (int y=0;y<col_of_m;y++)
+        { 
+          for (int z=0; z<page_of_m;z++) {
+            if (m[i,j,t]==m[x,y,z]) 
+            {
+               mcout[i,j,t]++;
+            }
+          
+          }
+        }
+       }
+      Console.WriteLine (mcout[i,j,t]);
+  }
+ }
+Console.WriteLine (" ");
+}
+
+Console.WriteLine(" ");
+Console.WriteLine(" ... ");
+Console.WriteLine(" ");
+
+for (int i=0;i<line_of_m;i++)
+{
+ for (int j=0;j<col_of_m;j++)
+  { 
+   for (int t=0; t<page_of_m; t++)
+   {
+     if ((mcout[i,j,t]==1)) {
+       Console.Write($" {mcopy[i,j,t]}{(i,j,t)} "); 
+     }
+   }
+    Console.WriteLine (" ");
+  }
+  Console.WriteLine (" ");
+}
+
+
+
+
+
 
 
 
